1f7fd4f568ff528b3675f34d65f6d64d6f7da9b1
Thomas <thomaslee9474@gmail.com>
2019-07-15 22:45:18 -0700

    [update] graph test

--- graph_test.py
+++ graph_test.py
@@ -8,18 +8,31 @@ class GraphTest(unittest.TestCase):
 
     def test_init(self):
         graph = Graph()
-        assert len(graph.vert_dict) == 0 
-        assert graph.num_vertices == 0
-        assert graph.num_edges == 0
+        self.assertEqual(len(graph.vert_dict), 0) 
+        self.assertEqual(graph.num_vertices, 0) 
+        self.assertEqual(graph.num_edges, 0) 
 
     def test_add_vertex(self):
         vertex_a = "A"
         graph = Graph()
         graph.add_vertex(vertex_a)
-        assert len(graph.vert_dict) == 1
+        self.assertEqual(len(graph.vert_dict), 1) 
         vertex_b = "B"
         graph.add_vertex(vertex_b)
-        assert len(graph.add_vertex) == 2
+        self.assertEqual(len(graph.vert_dict), 2)
+    
+    def test_get_vertex(self):
+        vertex_a = "A"
+        graph = Graph()
+        graph.add_vertex(vertex_a)
+        output_obj = graph.get_vertex(vertex_a)
+        self.assertIsInstance(output_obj, Vertex)
+    
+    def test_add_edge(self):
+        pass
+    
+    def get_vertices(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
