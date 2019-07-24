435f4fa24f85b76b0d59cc1f543ff8ea50ed971e
Thomas Lee <thomaslee9474@gmail.com>
2019-07-23 17:27:15 -0700

    [added] challenge 3

---
+++ challenge_3/classes/graph.py
@@ -0,0 +1,140 @@
+
+# !python
+
+from classes.vertex import Vertex
+from classes.stack import LinkedStack
+from classes.queue import LinkedQueue
+
+class Graph(object):
+    """ essential facts and functionalities of an undirected graph"""
+
+    def __init__(self):
+        """ 
+        initialises a graph object with an empty dict.
+        """
+
+        # vert_dict:
+        # {
+        #   "key": vertex_obj
+        # }
+
+        self.vert_dict = {}
+        self.num_vertices = 0
+        self.num_edges = 0
+
+    def add_vertex(self, key: str) -> object:
+        """
+        add a new vertex object to the graph with
+        the given key and return the vertex
+        """
+
+        if key not in self.vert_dict.keys():
+            # create new vertex and add to vertex dict
+            self.vert_dict[key] = Vertex(key)
+            # increment number of verticles
+            self.num_vertices += 1
+        return self.vert_dict[key]
+
+    def get_vertex(self, key: str)->object:
+        """return the vertex if it exists"""
+
+        return self.vert_dict[key]
+
+    def add_edge(self, vertex_a: str, vertex_b: str, weight=0):
+        """
+        add an edge from vertex a to vertex b with a weight
+        """
+        
+        # check if vertices exist in graph
+        if vertex_a not in self.vert_dict.keys():
+            # raise error if vertex_a does not exist
+            raise ValueError("This vertex does not exist in graph!")
+
+        if vertex_b not in self.vert_dict.keys():
+            # raise error if vertex_b does not exist
+            raise ValueError("This vertex does not exist in graph!")
+
+        vertex_a_obj = self.vert_dict[vertex_a]
+        vertex_b_obj = self.vert_dict[vertex_b]
+
+        # adding edges in both ways
+        vertex_a_obj.add_neighbour(vertex_b_obj, weight)
+        vertex_b_obj.add_neighbour(vertex_a_obj, weight)
+
+        # increment edge count
+        self.num_edges +=1 
+        
+
+    def get_vertices(self):
+        """return all the vertices in the graph"""
+
+        return self.vert_dict.keys()
+    
+    def get_edges(self, vertex: str) -> [str]:
+        """ Returns all of the edges of given vertex """
+        
+        return self.vert_dict[vertex].adj_dict_neighbours
+            
+    
+    def breadth_first_search(self, vertex_a: str, vertex_b: str)-> object:
+
+        """
+            Executes a breadth for search on the given graph. 
+
+            Args:
+                vertex_a: from vertex.
+                vertex_b: to vertex.
+            Returns:
+                a dict of containing parent and child verticies.
+        """
+
+        # Check if verticies exists in graph
+        if vertex_a not in self.vert_dict:
+            if vertex_b not in self.vert_dict:
+                raise ValueError("This vertex is not in graph!")
+
+        # Queue to keep track of verticies
+        # tail is front of queue
+        queue = LinkedQueue()
+
+        # Keeping track of visits
+        visited = set()
+
+        # create dict to store parent and children verticies
+        # parent = {
+        #   child_vertex: parent_vertex
+        # }
+        parent = dict()
+        
+        queue.enqueue(vertex_a)
+        visited.add(vertex_a)
+
+        # Iterating through queue
+        while not queue.is_empty():
+            # Dequeue front vertex
+            vertex = queue.dequeue()
+
+            for neighbour in self.vert_dict[vertex].adj_dict_neighbours:
+                if neighbour.id not in visited:
+                    # adding str's, not objects
+                    queue.enqueue(neighbour.id)
+                    visited.add(neighbour.id)
+                    # creating key-value pair in parent dict
+                    parent[neighbour.id] = vertex
+                    
+        return parent
+    
+def _traverse_pre_order_recursive(self, node, visit):
+        """Traverse this binary tree with recursive pre-order traversal (DFS).
+        Start at the given node and visit each node with the given function."""
+
+        # ! Runtime = O(n), must visit each node
+
+        # check to see if starting node contains data
+        if node is not None:
+            # Visit this node's data with given function
+            visit(node.data)
+            # Traverse left subtree, if it exists
+            self._traverse_pre_order_recursive(node.left, visit)
+            # Traverse right subtree, if it exists
+            self._traverse_pre_order_recursive(node.right, visit)
---
+++ challenge_3/classes/stack.py
@@ -0,0 +1,68 @@
+#!python
+
+from classes.linkedlist import LinkedList
+
+
+# Implement LinkedStack below, then change the assignment at the bottom
+# to use this Stack implementation to verify it passes all tests
+class LinkedStack(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new linked list to store the items
+        self.list = LinkedList()
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def __repr__(self):
+        """Return a string representation of this stack."""
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
+
+    def is_empty(self):
+        """Return True if this stack is empty, or False otherwise."""
+        # ll class method
+        return self.list.is_empty()
+
+    def length(self):
+        """Return the number of items in this stack."""
+        return self.list.length()
+
+    # ─── TOP IS HEAD ─────────────────────────────────────────────────
+
+    # ─── BOTTOM IS TAIL ──────────────────────────────────────────────
+
+    def push(self, item):
+        """Insert the given item on the top of this stack."""
+        # ! Best & worst case runtime = O(1), not dependent on length of ll
+
+        # append node to the tail
+        self.list.prepend(item)
+
+    def peek(self):
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        
+        # case: tail node is empty
+        if self.list.is_empty():
+            return None
+        # return tail data
+        return self.list.head.data
+
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty."""
+
+        # ! Worst & best case runtime = O(1), not dependent on length of ll
+        
+        # case: ll length is 0
+        if self.list.length() == 0:
+            raise ValueError('Linked list length is 0')
+        # case: top node is empty
+        if self.list.is_empty():
+            raise ValueError("Node is empty")
+        # delete tail node
+        item = self.list.head.data
+        self.list.delete(item)
+        return item
+
