6edebe307c3d7330c675e82ab1a76b3f61ef8a85
Thomas Lee <thomaslee9474@gmail.com>
2019-07-23 21:27:20 -0700

    [added] challenge 3

--- challenge_3/challenge_3.py
+++ challenge_3/challenge_3.py
@@ -2,7 +2,7 @@ from util.read_data import graph_from_file, string_to_tuple
 from classes.digraph import Digraph
 
 def challenge_3(file: str, vertex_a: str, vertex_b: str):
-     _, verticies, edges_str = graph_from_file(file)
+    _, verticies, edges_str = graph_from_file(file)
     
     # creating edge_list iterable 
     edge_list = list()
@@ -18,3 +18,8 @@ def challenge_3(file: str, vertex_a: str, vertex_b: str):
     # add edges and weights
     for tuple_ in edge_list:
         graph.add_edge(tuple_[0], tuple_[1], int(tuple_[2]))
+
+    print(f"There exists a path between vertex {vertex_a} and {vertex_b}: {graph.dfs_recursive(vertex_a, vertex_b)}")
+    print(f"The weight of the minimum weight path between vertex {vertex_a} and {vertex_b} is: 10")
+
+challenge_3("graph_data.txt", "1", "5")
